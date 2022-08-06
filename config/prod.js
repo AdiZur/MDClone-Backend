677f310daa8629821b15a86081813ca92bf15fbb
AdiZur <adizur9@gmail.com>
2022-08-06 19:26:00 +0300

    deploy to heroku

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://adizur:o7Lr75FzcqJBvyVC@cluster0.55jwi6x.mongodb.net/?retryWrites=true&w=majority',
 }
