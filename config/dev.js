1410d7e888dddbf0aa097e3d224a9bd10c918bdc
AdiZur <adizur9@gmail.com>
2022-08-06 21:16:57 +0300

    heroku

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // dbURL: 'mongodb://localhost:27017',
-  dbURL: 'mongodb+srv://adizur:o7Lr75FzcqJBvyVC@cluster0.55jwi6x.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: 'mongodb://localhost:27017',
+  // dbURL: 'mongodb+srv://adizur:o7Lr75FzcqJBvyVC@cluster0.55jwi6x.mongodb.net/?retryWrites=true&w=majority',
 }
