58b0b4aa0e0a034fe3d70d616682c5da63cbf9d8
AdiZur <adizur9@gmail.com>
2022-08-07 11:30:34 +0300

    atlas password

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   // dbURL: 'mongodb://localhost:27017',
-  dbURL: 'mongodb+srv://adizur:Eyxvzf0bZoHIa5aO@cluster0.55jwi6x.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://adizur:5cyTRbNoiGX6q2lq@cluster0.55jwi6x.mongodb.net/?retryWrites=true&w=majority',
 }
