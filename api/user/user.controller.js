0933d530b9db8c596604682bcf91792331880329
AdiZur <adizur9@gmail.com>
2022-08-07 13:26:49 +0300

    cleaning code

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,22 +1,9 @@
 const userService = require('./user.service')
 const logger = require('../../services/logger.service')
 
-// async function getUser(req, res) {
-//     try {
-//         const user = await userService.getById(req.params.id)
-//         res.send(user)
-//     } catch (err) {
-//         logger.error('Failed to get user', err)
-//         res.status(500).send({ err: 'Failed to get user' })
-//     }
-// }
-
 async function getUsers(req, res) {
     try {
-        const filterBy = {
-            //     txt: req.query?.txt || '',
-            //     minBalance: +req.query?.minBalance || 0
-        }
+        const filterBy = {}
         const users = await userService.query(filterBy)
         res.send(users)
     } catch (err) {
@@ -25,30 +12,6 @@ async function getUsers(req, res) {
     }
 }
 
-// async function deleteUser(req, res) {
-//     try {
-//         await userService.remove(req.params.id)
-//         res.send({ msg: 'Deleted successfully' })
-//     } catch (err) {
-//         logger.error('Failed to delete user', err)
-//         res.status(500).send({ err: 'Failed to delete user' })
-//     }
-// }
-
-// async function updateUser(req, res) {
-//     try {
-//         const user = req.body
-//         const savedUser = await userService.update(user)
-//         res.send(savedUser)
-//     } catch (err) {
-//         logger.error('Failed to update user', err)
-//         res.status(500).send({ err: 'Failed to update user' })
-//     }
-// }
-
 module.exports = {
-    // getUser,
-    getUsers,
-    // deleteUser,
-    // updateUser
+    getUsers
 }
--- middlewares/logger.middleware.js
+++ middlewares/logger.middleware.js
@@ -1,7 +1,5 @@
-// const logger = require('../services/logger.service')
 
 async function log(req, res, next) {
-  // logger.info('Sample Logger Middleware')
   next()
 }
 
