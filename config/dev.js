bff9469796f9e3334c347b9648888d0756059a2c
AdiZur <adizur9@gmail.com>
2022-08-07 12:10:29 +0300

    .

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb://localhost:27017',
-  // dbURL: 'mongodb+srv://adizur:Z6Xy70jGubYwlvei@cluster0.55jwi6x.mongodb.net/?retryWrites=true&w=majority',
+  // dbURL: 'mongodb://localhost:27017',
+  dbURL: 'mongodb+srv://adizur:Z6Xy70jGubYwlvei@cluster0.55jwi6x.mongodb.net/?retryWrites=true&w=majority',
 }
